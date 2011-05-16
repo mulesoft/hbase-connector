3cb886959e87c509b2f728679a52cfef892fd0c2
Franco Bulgarelli <franco@zaubersoftware.com>
2011-05-16 18:03:31 -0300

    Adding first demo

---
+++ example-add-weather-observation/src/test/java/org/mulesoft/demo/hbase/HBaseFunctionalTestDriver.java
@@ -0,0 +1,41 @@
+/**
+ * Mule HBase Cloud Connector
+ *
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+
+package org.mulesoft.demo.hbase;
+
+import org.mule.construct.SimpleFlowConstruct;
+import org.mule.tck.FunctionalTestCase;
+
+import java.util.Collections;
+
+public class HBaseFunctionalTestDriver extends FunctionalTestCase
+{
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "mule-config.xml";
+    }
+
+    /**
+     * Creates the resources necessary for the rest of the tests
+     */
+    public void setup() throws Exception
+    {
+        lookupFlowConstruct("AddWeatherObservation").process(getTestEvent(""));
+    }
+    
+
+    private SimpleFlowConstruct lookupFlowConstruct(final String name)
+    {
+        return (SimpleFlowConstruct) muleContext.getRegistry().lookupFlowConstruct(name);
+    }
+}
